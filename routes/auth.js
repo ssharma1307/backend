f15f9c154aea8ea0b8eef5dfc6da53ea0fcdfb76
ssharma1307 <sharma.sukhdeep30@gmail.com>
2023-08-01 17:27:02 -0400

    json print

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const User= require('../models/User');
 const router = express.Router();
-router.get('/',(req,res)=>{
 
-res.json()
-});
+
+
+//create a user using:POST "/api/auth". Does'nt require auth
+
+router.get('/',async (req,res)=>{
+
+console.log(req.body);
+const user = await User.create(req.body);
+user.save();
+res.send(req.body);
+
+})
 module.exports = router;
