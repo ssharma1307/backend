8d5bc98026c521785f822141266738a0f3dd6483
ssharma1307 <sharma.sukhdeep30@gmail.com>
2023-07-31 20:11:42 -0400

    INDEX UPDATED

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000
 
 //Available Routes
 app.use('/api/auth', require('./routes/auth'));
-app.use('/api/notes', require('./routes/notes'));
+app.use('/api/notes ', require('./routes/notes'));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
